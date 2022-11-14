a4ddfca424c993b93daf703f85a0da3606c58887
Asato Kazuchika <e225726@ie.u-ryukyu.ac.jp>
2022-11-14 19:14:12 +0900

    encapsulated Hero class and Enemy class

--- app/src/main/java/jp/ac/uryukyu/ie/e225726/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e225726/Enemy.java
@@ -9,10 +9,20 @@ package jp.ac.uryukyu.ie.e225726;
  * Created by tnal on 2016/11/13.
  */
 public class Enemy {
-    public String name;
-    public int hitPoint;
-    public int attack;
-    public boolean dead;
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
+
+    //nameのgetterメソッド
+    public String getName(){
+        return this.name;
+    }
+    //deadのgetterメソッド
+    public boolean getDead(){
+        return this.dead;
+    }
+
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
@@ -34,9 +44,11 @@ public class Enemy {
      * @param hero 攻撃対象
      */
     public void attack(Hero hero){
-        int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
-        hero.wounded(damage);
+        if (this.dead == false){
+            int damage = (int)(Math.random() * attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
+            hero.wounded(damage);
+        }
     }
 
     /**
--- app/src/main/java/jp/ac/uryukyu/ie/e225726/Hero.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e225726/Hero.java
@@ -9,10 +9,24 @@ package jp.ac.uryukyu.ie.e225726;
  * Created by tnal on 2016/11/13.
  */
 public class Hero {
-    public String name;
-    public int hitPoint;
-    public int attack;
-    public boolean dead;
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
+
+    //nameのgetter
+    public String getName(){
+        return this.name;
+    }
+    //hitPointのgetter
+    public int getHitPoint(){
+        return this.hitPoint;
+    }
+    //deadのgetter
+    public boolean getDead(){
+        return this.dead;
+    }
+    
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
@@ -35,7 +49,7 @@ public class Hero {
      */
     public void attack(Enemy e){
         int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.name, damage);
+        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.getName(), damage);
         e.wounded(damage);
     }
 
